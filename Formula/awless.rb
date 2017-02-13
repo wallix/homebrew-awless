6d0664d8f9fba661785cc9d715d3d8a60d1f7242
François-Xavier Aguessy <fxaguessy@users.noreply.github.com>
2017-02-13 16:34:03 +0100

    Add brew formula to install awless

---
+++ Formula/awless.rb
@@ -0,0 +1,47 @@
+class Awless < Formula
+  desc "The Mighty CLI for AWS"
+  homepage "https://github.com/wallix/awless"
+  url "https://github.com/wallix/awless.git",
+        :tag => "0.0.10",
+        :revision => "1ff7af712f2e7a6c3665060d23037ca3162c3a11"
+  head "https://github.com/wallix/awless.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    arch = MacOS.prefer_64_bit? ? "amd64" : "x86"
+    dir = buildpath/"src/github.com/wallix/awless"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      # Make binary
+      system "go", "run", "release.go", "-tag","0.0.10","-brew","-arch","#{arch}", "-os","darwin"
+      bin.install "awless"
+
+      # Install bash completion
+      output = Utils.popen_read("#{bin}/awless completion bash")
+      (bash_completion/"awless").write output
+      
+      # Install zsh completion
+      output = Utils.popen_read("#{bin}/awless completion zsh")
+      (zsh_completion/"_awless").write output
+    end
+  end
+  
+  def caveats
+      <<-EOS.undent
+      
+      In order to get awless completion, 
+        [bash] you need to install `bash-completion` with brew.
+        OR
+        [zsh], add the following line to your ~/.bashrc:
+          source #{HOMEBREW_PREFIX}/share/zsh/site-functions/_awless
+      EOS
+  end
+
+  test do
+    run_output = shell_output("#{bin}/awless --help 2>&1")
+    assert_match "Awless is a powerful command line tool to inspect, sync and manage your infrastructure", run_output
+  end
+end
